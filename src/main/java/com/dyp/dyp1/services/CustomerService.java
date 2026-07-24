211e056ad94cc5cae24f4d4c6237c03eca63b2b0
Nikunj Sondawale <3nikunjsondawale@gmail.com>
2026-07-24 12:21:51 +0530

    Customer service and controller

--- src/main/java/com/dyp/dyp1/services/CustomerService.java
+++ src/main/java/com/dyp/dyp1/services/CustomerService.java
@@ -1,5 +1,60 @@
 package com.dyp.dyp1.services;
 
-public class CustomerService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dyp.dyp1.entity.Customer;
+import com.dyp.dyp1.repositories.CustomerRepo;
+
+@Service
+public class CustomerService {
+	@Autowired
+	private CustomerRepo cust_repo;
+	
+	public void saveCust(Customer cust) {
+		cust_repo.save(cust);
+	}
+	
+	public List<Customer> getAllCust(){
+		return cust_repo.findAll();
+	}
+	
+	public Optional<Customer> getCust(Long id) {
+		return cust_repo.findById(id);
+	}
+	
+	public Customer updateCust(Customer cust) {
+		Customer old_cust =  cust_repo.getById(cust.getId());
+		
+		old_cust.setEmail(cust.getEmail());
+		old_cust.setName(cust.getName());
+		old_cust.setPhone(cust.getPhone());
+		
+		cust_repo.save(old_cust);
+		return old_cust;
+	}
+	
+	public void deleteCust(Long id) {
+		cust_repo.deleteById(id);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

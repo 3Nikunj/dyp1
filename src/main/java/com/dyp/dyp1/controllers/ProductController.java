ff0b4d91e8bc68ff7d963d9cdc93bfdd376107cb
Nikunj Sondawale <3nikunjsondawale@gmail.com>
2026-07-24 10:39:01 +0530

    Update ProductController.java

--- src/main/java/com/dyp/dyp1/controllers/ProductController.java
+++ src/main/java/com/dyp/dyp1/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,11 @@ public class ProductController {
 	@GetMapping("/getall")
 	public List<Product> getAll(){
 		return pro_serv.getAllProducts();
+	}
+	
+	@PutMapping("/update")
+	public Product update(@RequestBody Product pro) {
+		return pro_serv.updateProduct(pro);
 	}
 	
 	
